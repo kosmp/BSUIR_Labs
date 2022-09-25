8ae0da5974376ca7ee65608ba13a737ece2c81ab
Pavel <kosmin.2003@mail.ru>
2022-09-25 15:38:42 +0300

    Added Lab7 3-term C#

---
+++ 3-term(C#)/Lab7/Lab7/Program.cs
@@ -0,0 +1,128 @@
+﻿using Lab7;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TestClass test1 = new TestClass();
+        Console.CursorVisible = false;
+
+        List<string> listOfResults = new List<string>();
+        test1.NotifyEnd += (result, time) =>
+        {
+            listOfResults.Add("Thread ID: " + Thread.CurrentThread.ManagedThreadId + " Result of calc: " + result + " Time: " + time + " CurrentThreadName: " + Thread.CurrentThread.Name);
+        };
+
+        (int id, int progress)[] progressArr = new (int id, int progress)[5] { (0, 0), (0, 0), (0, 0), (0, 0), (0, 0) };
+        test1.NotifyInProcess += (currProgress, currThreadID) =>
+        {
+            bool idIsInArr = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (progressArr[i].id == currThreadID)
+                {
+                    progressArr[i].progress = currProgress;
+                    idIsInArr = true;
+                    break;
+                }
+            }
+
+            if (idIsInArr == false)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (progressArr[i].id == 0)
+                    {
+                        progressArr[i].id = currThreadID;
+                        progressArr[i].progress = currProgress;
+                        break;
+                    }
+                }
+            }
+
+            Console.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int k = 0; k <= progressArr[i].progress; k++) // draws all 5 as much as they were ready
+                {
+                    if (k == 0)
+                    {
+                        Console.Write("\nThread:" + progressArr[i].id + " [");
+                        Console.Write($"{k}%");
+                        Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+                    }
+                    else if (k == 100)
+                    {
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        Console.Write("=]");
+                        Console.Write($"{progressArr[i].progress}%");
+                    }
+                    else if (k != 100)
+                    {
+                        Console.Write($"=>{k}%");
+                        string sCurrProgress = k.ToString();
+                        Console.SetCursorPosition(Console.CursorLeft - sCurrProgress.Length - 2, Console.CursorTop);
+                    }
+                }
+            }
+
+            int finalCounter = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (progressArr[i].progress == 100)
+                    finalCounter++;
+            }
+
+            if (finalCounter == 5)
+            {
+                Console.Write("\n");
+                foreach (var item in listOfResults)
+                    Console.WriteLine(item.ToString());
+            }
+        };
+
+        Thread thread1 = new Thread(new ThreadStart(test1.calculateSinIntegral));
+        thread1.Name = "FirstThread";
+        thread1.Priority = ThreadPriority.Lowest;
+        thread1.Start();
+
+        Thread thread2 = new Thread(new ThreadStart(test1.calculateSinIntegral));
+        thread2.Name = "SecondThread";
+        thread2.Priority = ThreadPriority.Highest;
+        thread2.Start();
+
+        Thread thread3 = new Thread(new ThreadStart(test1.calculateSinIntegral));
+        thread3.Name = "ThirdThread";
+        thread3.Start();
+
+        Thread thread4 = new Thread(new ThreadStart(test1.calculateSinIntegral));
+        thread4.Name = "FourthThread";
+        thread4.Start();
+
+        Thread thread5 = new Thread(new ThreadStart(test1.calculateSinIntegral));
+        thread5.Name = "FifthThread";
+        thread5.Start();
+
+        // if one by one
+        //test1.NotifyInProcess += (progress, threadID) =>
+        //{
+        //    if (progress == 1)
+        //    {
+        //        Console.Write("\nThread:" + threadID + " [");
+        //    }
+
+        //    if (progress != 100)
+        //    {
+        //        Console.Write("=>");
+        //        int startPos = Console.CursorLeft;
+        //        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+        //        Console.Write($"{progress}%");
+        //        Console.SetCursorPosition(startPos - 1, Console.CursorTop);
+        //    }
+        //    else
+        //    {
+        //        Console.Write($"]{progress}%\n");
+        //    }
+        //};
+    }
+}
---
+++ 3-term(C#)/Lab7/Lab7/TestClass.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace Lab7
+{
+    public class TestClass
+    {
+        static Semaphore sem = new Semaphore(2, 5);
+        static Mutex mtx = new Mutex();
+
+        public delegate void CulcHandler(double computedIntegral, string funcExecutionTime);
+        public event CulcHandler NotifyEnd;
+        public delegate void ProcessHandler(int progress, int threadID);
+        public event ProcessHandler NotifyInProcess;
+
+        public void calculateSinIntegral()
+        {
+            sem.WaitOne();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            double h = 0.00000001;  // step
+            double res = 0;
+            for (double i = 0; i < 1.00000001; i += 0.00000001)
+            {
+                res += h * Math.Sin(i);
+                for (int k = 1; k <= 100000; k++) ;  // from condition
+
+                int t = Convert.ToInt32(i * 100000000);
+                if (t != 0 && (t % 1000000 == 0 || t % 100000000 == 0)) // checking if i is two digit int
+                {
+                    mtx.WaitOne();
+                    NotifyInProcess?.Invoke(Convert.ToInt32(i * 100), Thread.CurrentThread.ManagedThreadId);
+                    mtx.ReleaseMutex();
+                }
+                else if (t == 0)
+                {
+                    mtx.WaitOne();
+                    NotifyInProcess?.Invoke(Convert.ToInt32(0), Thread.CurrentThread.ManagedThreadId);
+                    mtx.ReleaseMutex();
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            NotifyEnd?.Invoke(res, elapsedTime);
+            sem.Release();
+        }
+    }
+}

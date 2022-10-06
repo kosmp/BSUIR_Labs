957c6d66f09e64fdaf9136491ab12bc6254c673f
Pavel <kosmin.2003@mail.ru>
2022-10-06 10:13:17 +0300

    Added labs 1-3

---
+++ 3-term(Assembler)/Lab1/Lab1.asm
@@ -0,0 +1,13 @@
+ org $8000
+ xgdx
+ xgdy
+ xgdx
+ stx $904
+ sty $853
+ ldy $904
+ ldx $853
+ pshx
+ pshy
+ pulx
+ puly
+ 
---
+++ 3-term(Assembler)/Lab3/Lab3.asm
@@ -0,0 +1,19 @@
+ org $8000
+ ldaa #%11101010
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ rora
+ rolb
+ eorb #%01010101

be9cff7d8989764e03aa748726a828935b5bbc39
Pavel <kosmin.2003@mail.ru>
2023-04-04 14:50:23 +0300

    fix lab3 .NET MAUI

--- 4-term(C#)/MauiApp/MauiApp1/CocktailStorage.xaml.cs
+++ 4-term(C#)/MauiApp/MauiApp1/CocktailStorage.xaml.cs
@@ -1,34 +1,43 @@
 using MauiApp1.Entities;
 using MauiApp1.Services;
 using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
 
 namespace MauiApp1;
 
 public partial class CocktailStorage : ContentPage
 {
-    private readonly SQLiteService db;
+    private readonly IDbService db;
 
-    public ObservableCollection<Ingredient> Ingredients { get; set; }
-    public ObservableCollection<Cocktail> Cocktails { get; set; }
+    public ObservableCollection<Ingredient> Ingredients { get; set; } = new();
+    public ObservableCollection<Cocktail> Cocktails { get; set; } = new();
 
     public CocktailStorage(IDbService dbService)
     {
         InitializeComponent();
 
-        db = dbService as SQLiteService;
+        db = dbService;
+
         db.Init();
         BindingContext = this;
     }
 
     private void PickerSelectedIndexChanged(object sender, EventArgs e)
     {
-        Ingredients = new ObservableCollection<Ingredient>(db.GetCocktailIngredients((picker.SelectedItem as Cocktail).Id));
-        IngredientListView.ItemsSource = Ingredients;
+        Ingredients.Clear();
+        var ing = db.GetCocktailIngredients((picker.SelectedItem as Cocktail).Id);
+        foreach(var item in ing)
+        {
+            Ingredients.Add(item);
+        };
     }
 
     private void OnPage_Loaded(object sender, EventArgs e)
     {
-        Cocktails = new ObservableCollection<Cocktail>(db.GetAllCocktails());
-        picker.ItemsSource = Cocktails;
+        Cocktails.Clear();
+        foreach (var item in db.GetAllCocktails())
+        {
+            Cocktails.Add(item);
+        };
     }
 }
